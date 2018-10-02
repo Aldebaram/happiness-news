c0b389d005fd448647e729e2bb24a260cb23dba5
BlueReptile <callmekalango@gmail.com>
2018-10-02 11:22:58 -0300

    create a instance of materialize character counter

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 function goBack() {
-  window.history.back();
+  window.history.back(); //go back to previous page
 }
 
 function loading() {
   document.getElementById("second").innerHTML +=
-    "<div class=\"progress\"><div class=\"indeterminate\"></div></div>";
+    "<div class=\"progress\"><div class=\"indeterminate\"></div></div>"; //insert loading bar
   document.getElementById("form").submit(); // Form submission
 }
+
+
+// initialize materialize character count
+const textNeedCount = document.querySelector('#content');
+const textNeedCountInstance = new M.CharacterCounter(textNeedCount);
